5323f3789cbe2f45de14afe475f1a81c3bc2d437
Roee <Roee.eldar131@gmail.com>
2025-12-06 20:28:07 +0200

    added tree interface

--- AvlTree.h
+++ AvlTree.h
@@ -1,16 +1,28 @@
-//
-// Created by Roee on 12/6/2025.
-//
+#pragma once
 
-#ifndef AVLTREE_H
-#define AVLTREE_H
-
-
+template <typename KeyType, typename ValueType>
 
 class AvlTree {
+private:
+    struct Node {
+        KeyType key;
+        ValueType value;
+        Node* parent;
+        Node* left;
+        Node* right;
+        int height; // to calc balance factor
+    };
+    Node* root = nullptr;
+
+    // rolls...
+    // blance factor calc
+    // helper function to destroy tree starting from root - if using normal pointers
+
+
+public:
+    Node* find (const KeyType& key) const; // doesnt change tree, might change node
+    // if return as not smart pointer, others might access it after earased..
+    bool insert (const KeyType& key, const ValueType& value); // false if exists
+    bool erase (const KeyType& key); // false if doesnt exist
 
 };
-
-
-
-#endif //AVLTREE_H
