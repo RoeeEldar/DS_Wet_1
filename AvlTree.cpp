c6abd3b796b782f9f9ced5818e5d52814e2bbcd8
Roee Eldar <roee.eldar131@gmail.com>
2025-12-06 18:02:16 +0200

    updated git ignore

---
+++ AvlTree.cpp
@@ -0,0 +1,6 @@
+
+#include "AvlTree.h"
+int main() {
+    return 0;
+
+}
---
+++ AvlTree.h
@@ -0,0 +1,16 @@
+//
+// Created by Roee on 12/6/2025.
+//
+
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+
+
+class AvlTree {
+
+};
+
+
+
+#endif //AVLTREE_H
